8dba1264867856e8d482178e7cb3df152d68e653
Aliaksei Miadzvetski <123022357+GhosteLLoS@users.noreply.github.com>
2023-08-08 22:43:19 +0200

    Add files via upload

---
+++ HW_11/src/database/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Contact(Base):
+    table_name = "contacts"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50))
+    phone = Column(String(50))
+    birthdate = Column(Date)
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref='contacts')
+
+
+class User(Base):
+    table_name = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(500), nullable=False, unique=True)
+    password = Column(String(500), nullable=False)
+    refresh_token = Column(String(500), nullable=True)
