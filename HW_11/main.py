b2943342b02588dbd8c019a94df0c1231ff522e9
Aliaksei Miadzvetski <123022357+GhosteLLoS@users.noreply.github.com>
2023-08-09 22:39:28 +0200

    Delete HW_11 directory

--- HW_11/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from src.routes import contacts, auth
-
-app = FastAPI()
-
-app.include_router(contacts.router, prefix='/api')
-app.include_router(auth.router, prefix='/api')
-
